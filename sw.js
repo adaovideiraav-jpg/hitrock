15943fc010db969f63de1c8106358e141a213132
adaovideiraav-jpg <adao.videira.av@gmail.com>
2026-09-23 23:30:35 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='hitrock-v1.81';
+const C='hitrock-v1.82';
 const CORE=['./songs.json','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
